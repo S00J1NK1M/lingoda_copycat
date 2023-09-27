95d5e3503eba7bb0713db257b00f0f77ac427c22
Soojin Kim <contact.soojin.kim@gmail.com>
2023-09-27 15:06:22 +0200

    created fake seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,37 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "Cleaning database..."
+# Language.destroy_all
+# Teacher.destroy_all
+
+# puts "Creating db..."
+
+# # create languages
+# italian = { name: "Italian" }
+# ukranian = { name: "Ukranian" }
+
+# [italian, ukranian].each do |attributes|
+#   language = Language.create!(attributes)
+#   puts "Created #{language.name}"
+# end
+
+# # create teachers
+# matteo = { name: "Matteo" }
+# anna = { name: "Anna" }
+
+# [matteo, anna].each do |attributes|
+#   teacher = Teacher.create!(attributes)
+#   puts "Created #{teacher.name}"
+# end
+
+# create courses
+italian_A1 = { description: "test test", level: "A1", language_id: 15, teacher_id: 12 }
+ukranian_A2 = { description: "test test testttt", level: "A2", language_id: 16, teacher_id: 13}
+
+[italian_A1, ukranian_A2].each do |attributes|
+  course = Course.create!(attributes)
+  puts "Created!"
+end
+
+puts "Finished!"
