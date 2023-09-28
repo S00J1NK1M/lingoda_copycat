8396b89ee27f16b94a9f603a21b1bad1b29825b6
Anna Boiko <annaboiko1@icloud.com>
2023-09-28 09:34:42 +0200

    add css

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,30 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Cleaning database..."
-# Language.destroy_all
-# Teacher.destroy_all
+Language.destroy_all
+Teacher.destroy_all
 
-# puts "Creating db..."
+puts "Creating db..."
 
-# # create languages
-# italian = { name: "Italian" }
-# ukranian = { name: "Ukranian" }
+# create languages
+italian = { name: "Italian" }
+ukranian = { name: "Ukranian" }
 
-# [italian, ukranian].each do |attributes|
-#   language = Language.create!(attributes)
-#   puts "Created #{language.name}"
-# end
+[italian, ukranian].each do |attributes|
+  language = Language.create!(attributes)
+  puts "Created #{language.name}"
+end
 
-# # create teachers
-# matteo = { name: "Matteo" }
-# anna = { name: "Anna" }
+# create teachers
+matteo = { name: "Matteo" }
+anna = { name: "Anna" }
 
-# [matteo, anna].each do |attributes|
-#   teacher = Teacher.create!(attributes)
-#   puts "Created #{teacher.name}"
-# end
+[matteo, anna].each do |attributes|
+  teacher = Teacher.create!(attributes)
+  puts "Created #{teacher.name}"
+end
 
-# create courses
+create courses
 italian_A1 = { description: "test test", level: "A1", language_id: 15, teacher_id: 12 }
 ukranian_A2 = { description: "test test testttt", level: "A2", language_id: 16, teacher_id: 13}
 
