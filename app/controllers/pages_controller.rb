c2ae5bd0e4e45f51854f2473800ac81533a2c384
Soojin Kim <contact.soojin.kim@gmail.com>
2023-10-04 14:07:28 +0200

    working on filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,9 @@ class PagesController < ApplicationController
   def home
     # Read All
     @courses = Course.all
+    @languages = @courses.map { |course| course.language.name }
+
+    @languages_filtered = @languages.uniq
+    raise
   end
 end
